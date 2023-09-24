be13736852bfc926cd1e45a7006a8dd0caf69c0e
Souhaib <souhaib.benbouzid@gmail.com>
2023-09-24 12:17:19 +0100

    two sums improved

--- src/1. Two Sum.ts
+++ src/1. Two Sum.ts
@@ -1,8 +1,9 @@
 /* 
     Difficulty: easy
-    Solution Complexity: O(n2)
+    Solution Complexity: O(n)
 */
 
+// O(n2)
 function twoSum(nums: number[], target: number): number[] {
   let arr: number[] = [];
   for (let i = 0; i < nums.length; i++) {
@@ -17,6 +18,26 @@ function twoSum(nums: number[], target: number): number[] {
   return arr;
 }
 
+// O(n)
+function fastTwoSum(nums: number[], target: number): number[] {
+  // diff -> index
+  const differenceMap = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    console.log(differenceMap);
+    // number in diffMap ?
+    if (differenceMap.get(target - nums[i]) !== undefined) {
+      return [differenceMap.get(target - nums[i]), i];
+    } else {
+      // add diff to Map
+      differenceMap.set(nums[i], i);
+    }
+  }
+
+  return [];
+}
+
 let nums = [3, 2, 4];
 
+console.log(fastTwoSum(nums, 6));
 console.log(twoSum(nums, 6));
