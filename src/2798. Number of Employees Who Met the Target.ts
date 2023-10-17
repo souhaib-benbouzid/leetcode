402a7b4336f62c89d1cf1fda744118ffde31b15c
Souhaib <souhaib.benbouzid@gmail.com>
2023-10-17 20:56:08 +0100

    2798. Number of Employees Who Met the Target

---
+++ src/2798. Number of Employees Who Met the Target.ts
@@ -0,0 +1,12 @@
+function numberOfEmployeesWhoMetTarget(
+  hours: number[],
+  target: number
+): number {
+  let count = 0;
+  for (let i = 0; i < hours.length; i++) {
+    if (hours[i] >= target) {
+      count++;
+    }
+  }
+  return count;
+}
