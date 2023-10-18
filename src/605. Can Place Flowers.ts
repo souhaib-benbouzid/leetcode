62606adf903c69d573d4cc97d7818ae442bc8b9c
Souhaib <souhaib.benbouzid@gmail.com>
2023-10-18 19:13:12 +0100

    605. Can Place Flowers

---
+++ src/605. Can Place Flowers.ts
@@ -0,0 +1,12 @@
+function canPlaceFlowers(flowerbed: number[], n: number): boolean {
+  let count = 0;
+
+  for (let i = 0; i < flowerbed.length; i++) {
+    if (!flowerbed[i - 1] && !flowerbed[i] && !flowerbed[i + 1]) {
+      count++;
+      flowerbed[i] = 1;
+    }
+  }
+
+  return count >= n;
+}
